175e23aefc590f6b7ee9133fa00264a38219240d
David Thomas <dave_thomas@enoughwebapp.com>
2023-07-06 17:39:43 -0500

    finished up basic code and chat structure

--- main.py
+++ main.py
@@ -6,16 +6,18 @@
 import streamlit as st
 from PIL import Image
 from utils.chat_utils import ChatService
+from streamlit_chat import message
+import asyncio
 
 # Establish the initial session state
 # Create a function to initialize the session state variables
 def init_session_variables():
     # Initialize session state variables
     session_vars = [
-        'home_page', 'user_name', 'user_age', 'topic', 'teacher', 'chat_history', 'chat_service'
+        'home_page', 'user_name', 'user_age', 'topic', 'teacher', 'chat_messages', 'chat_service', 'initial_question', 'i', 'difficulty_score', 'is_correct', 'total_score'
     ]
     default_values = [
-        'get_user_info', '', '', '', '', [], ChatService()
+        'get_user_info', '', '', '', '', [], ChatService(), '', 0, 0, False, 0
     ]
 
     # Loop through the session variables and set them if they are not already set
@@ -23,25 +25,39 @@ def init_session_variables():
         if session_var not in st.session_state:
             st.session_state[session_var] = default_value
 
-init_session_variables
 # Define a function to get the user information
 def get_user_info():
+    # Display an image of the Paw Patrol characters
+    st.image(Image.open("resources/paw_patrol1.jpg"))
+    st.markdown("##### Welcome to paw patrol school!  Let's get started!")
+    st.text("")
     # Get the user name via a text input
-    st.session_state.user_name = st.text_input("What is your name?")
+    st.session_state.user_name = st.selectbox("Who is learning today?", ["Archie", "LuAnna"])
     # Get the user age via a text input
-    st.session_state.user_age = st.text_input("What is your age?")
-    # Change the page state to get the topic and teacher selections
-    st.session_state.home_page = 'get_topic_and_teacher'
-    # Reset the script
-    st.experimental_rerun()
+    #st.session_state.user_age = st.text_input("What is your age?")
+    # Create a button to submit the user info
+    initial_info_button = st.button("Submit", type='primary', use_container_width=True)
+    # If the user clicks the button, change the page state to get the topic and teacher selections
+    if initial_info_button:
+        if st.session_state.user_name == "Archie":
+            st.session_state.user_age = 4
+        else:
+            st.session_state.user_age = 6
+        st.session_state.home_page = 'get_topic_and_teacher'
+        # Reset the script
+        st.experimental_rerun()
 
 # Define a function to get the topic and teacher selections
 def get_topic_and_teacher():
     # Greet the user by name
-    st.write(f"Hi {st.session_state.user_name}!")
-    st.markdown("Which teacher would you like to choose today?")
+    st.markdown(f"**Hi {st.session_state.user_name}!  What would you like to learn about today?**")
+    # Get the topic selection from the user
+    topic = st.text_input("Enter a topic:")
+    # Get the teacher selection from the user
+    st.markdown("**Which teacher would you like to choose today?**")
+    st.text("")
     # Ask the user which teacher they would like to choose -- we will use two columns to display the teacher pics and buttons to choose them with
-    col1, col2 = st.columns(2)
+    col1, col2 = st.columns(2, gap="large")
     # Define the teacher images 
     teacher_images = {
         "Chase" : Image.open("resources/chase1.jpg"),
@@ -50,6 +66,85 @@ def get_topic_and_teacher():
     # Col1 will be Chase
     with col1:
         st.image(teacher_images["Chase"])
-        if st.button("Chase"):
-            st.session_state.teacher = "Chase"
+        if st.button("Chase", type='primary', use_container_width=True):
+            st.session_state.chat_service.teacher = "Chase"
+            st.session_state.chat_service.topic = topic
+            st.session_state.chat_service.age = st.session_state.user_age
+            st.session_state.chat_service.kid_name = st.session_state.user_name
+            st.session_state.chat_service.get_initial_message_from_teacher()
+            st.session_state.home_page = 'chat_session'
+            st.experimental_rerun()
+
+    # Col2 will be Leo
+    with col2:
+        st.image(teacher_images["Leo"])
+        if st.button("Leo", type='primary', use_container_width=True):
+            st.session_state.chat_service.teacher = "Leo"
+            st.session_state.chat_service.topic = topic
+            st.session_state.chat_service.age = st.session_state.user_age
+            st.session_state.chat_service.kid_name = st.session_state.user_name
+            st.session_state.chat_service.get_initial_message_from_teacher()
+            st.session_state.home_page = 'chat_session'
+            st.experimental_rerun()
+
+# Define a function to display the chat message
+def display_chat_message(chat_message, index):
+    if chat_message['role'] == 'user':
+        message(chat_message['content'], avatar_style="initials", seed="U", is_user=True, key = f"chat_message_{index}")
+    elif chat_message['role'] == 'ai':
+        message(chat_message['content'], avatar_style="initials", seed="SC", key = f"chat_message_{index}")
+
+# Define a function to handle the user's input
+def handle_user_input():
+    # Create a text input for the user to enter their response
+    user_response = st.text_input("Enter your response:")
+    # Create a button to submit the user response
+    submit_response_button = st.button("Submit")
+    # If the user clicks the button, get the response from the user and send it to the chat service
+    if submit_response_button:
+        with st.spinner(f'{st.session_state.chat_service.teacher} is thinking...'):
+            st.session_state.chat_service.get_answer_from_teacher(user_response)
+            st.experimental_rerun()
+
+# Define a function to run the chat session
+def chat_session():
+    # Create two columns -- one to display the teacher's image and one to display the chat messages
+    col1, col2 = st.columns([1, 2.5], gap='large')
+    # Col1 will be the teacher's image -- we need to check if the teacher is Chase or Leo
+    with col1:
+        if st.session_state.chat_service.teacher == "Chase":
+            st.image(Image.open("resources/chase1.jpg"))
+        elif st.session_state.chat_service.teacher == "Leo":
+            st.image(Image.open("resources/leo1.jpg"))
+    # Col2 will be the chat messages
+    with col2:
+        # Only display the last message
+        last_message = st.session_state.chat_messages[-1]
+        last_index = len(st.session_state.chat_messages) - 1
+        display_chat_message(last_message, last_index)
+        handle_user_input()
+
+
+    #if st.session_state.is_correct == True:
+    #        st.markdown(f'##### Congratulations, {st.session_state.chat_service.kid_name}🎉!  You earned {st.session_state.difficulty_score} points for that\
+    #            correct answer!  Your total score is {st.session_state.total_score} points. Keep up the good work!  🤩')
+
+    
+
+
+
+# Define the flow of the app
+def main():
+    init_session_variables()
+
+    if st.session_state.home_page == 'get_user_info':
+        get_user_info()
+    elif st.session_state.home_page == 'get_topic_and_teacher':
+        get_topic_and_teacher()
+    elif st.session_state.home_page == 'chat_session':
+        chat_session()
+
+# Run the app
+if __name__ == "__main__":
+    main()
        
--- utils/chat_utils.py
+++ utils/chat_utils.py
@@ -6,6 +6,17 @@ import openai
 import os
 from dotenv import load_dotenv
 import streamlit as st
+from langchain.prompts import (
+    PromptTemplate,
+    ChatPromptTemplate,
+    SystemMessagePromptTemplate,
+    #AIMessagePromptTemplate, 
+    HumanMessagePromptTemplate
+)
+from langchain.chat_models import ChatOpenAI
+from pydantic import BaseModel, Field
+from typing import List, Optional
+from langchain.output_parsers import PydanticOutputParser
 
 load_dotenv()
 
@@ -13,6 +24,14 @@ load_dotenv()
 # Set the API key
 openai.api_key = os.getenv("OPENAI_API_KEY")
 openai.organization = os.getenv("OPENAI_ORG")
+
+# Define a pydantic object to format the response from the model
+class TeacherResponse(BaseModel):
+    text: str = Field(description="The text response from the teacher")
+    #difficulty_score: int = Field(description="The difficulty score of the question")
+    #is_correct: bool = Field(description="Whether the user's answer is correct or not")
+
+output_parser = PydanticOutputParser(pydantic_object=TeacherResponse)
 
 
 # Define a ChatMessage class to handle formatting the messages
@@ -25,23 +44,19 @@ class ChatMessage:
 # Define a class to handle the chatbot using OpenAI and langchain
 class ChatService:
     # Initialize the chatbot with the initial message depending on the context
-    def __init__(self, teacher: str, topic: str, age: str, kid_name: str):
+    def __init__(self):
         # Initialize the chat history
         self.chat_history = ChatMessageHistory()
         # Initialize the chat history dictionary
         self.chat_history_dict = {}
-        # Initialize the initial message
-        self.initial_system_message = {}
         # Initialize the teacher selecteds
-        self.teacher = teacher
+        self.teacher = ""
         # Initialize the topic selected
-        self.topic = topic
+        self.topic = ""
         # Initialize the age of the student
-        self.age = age
+        self.age = ""
         # Initialize the name of the student
-        self.kid_name = kid_name
-        # Initialize the initial messages
-        self.initial_messages = []
+        self.kid_name = ""
         # Initialize the chat_messages
         self.chat_messages = []
       
@@ -58,165 +73,98 @@ class ChatService:
         # Get the latest message
         latest_message = chat_messages[-1]
         # Convert the response to a Message object
-        formatted_message = ChatMessage(role = "user", content = latest_message[0]['data']['content'])
-        # Append the message to the chat history
-        st.session_state.chat_service.chat_messages.append({"role": formatted_message.role, "content": formatted_message.content})
-        # Return the chat history
-        return st.session_state.chat_service.chat_messages
-        
-
-    def add_teacher_message(self, message: str):
-        self.chat_history.add_ai_message(message)
-        # Return the latest message in the chat history
-        chat_messages =  self.save_chat_history_dict()
-        # Get the latest message
-        latest_message = chat_messages[-1]
-        # Convert the response to a Message object
-        formatted_message = ChatMessage(role = "ai", content = latest_message[0]['data']['content'])
-        # Append the message to the chat history
-        self.chat_messages.append({"role": formatted_message.role, "content": formatted_message.content})
-        # Return the chat history
-        return self.chat_messages
-    
-    def add_user_message(self, message):
-        response = requests.post(f"{self.baseUrl}/add_user_message", params={"message": message})
-        data = response.json()
-        # Convert the response to a Message object
-        message = ChatMessage(role = "user", content = data[0]['data']['content'])
-
-        # Append the message to the chat history
-        self.chat_messages.append({"role": message.role, "content": message.content})
-
-        # Return the chat history
-        return self.chat_messages
+        try:
+            formatted_message = ChatMessage(role = "user", content = latest_message[0]['data']['content'])
+        except:
+            formatted_message = ChatMessage(role = "user", content = latest_message['data']['content'])
+        if formatted_message:
+            #Append the message to the chat history
+            st.session_state.chat_messages.append({"role": formatted_message.role, "content": formatted_message.content})
+            # Return the chat history
+            return st.session_state.chat_messages
     
     
-    # Define the function to get the initial message from the teacher.  This will be the first message to initiate
-    # the socratic method conversation with the student based on the topic and age
     def get_initial_message_from_teacher(self):
-        # Define the messages to pass to the OpenAI API
         messages = [
             {
                 "role": "system",
-                "content": f"You are a teacher that is helping a child learn about different topics based on the socratic method.\
-                    You have taken on the personality of {self.teacher} from the kids show 'Paw Patrol'.  The child's name is {self.kid_name}.\
-                    The topic you are teaching is {self.topic}.  The age of the child is {self.age}.  The format of the conversation is question / response,\
-                    where you ask a question and the child gives a response.  Please give the first question to the child to start the conversation."
-            },
-        ]
-           # Use the OpenAI API to generate a recipe
-        try:
-            response = openai.ChatCompletion.create(
-            model="gpt-3.5-turbo-16k-0613",
-            messages=messages,
-            max_tokens=750,
-            frequency_penalty=0.5,
-            presence_penalty=0.5,
-            temperature=1,
-            top_p=0.9,
-            n=1,
-        )
-            response = response.choices[0].message.content
-
-        except (requests.exceptions.RequestException, openai.error.APIError):
-            response = openai.ChatCompletion.create(
-            model="gpt-3.5-turbo-16k",
-            messages=messages,
-            max_tokens=750,
-            frequency_penalty=0.5,
-            presence_penalty=0.5,
-            temperature=1,
-            top_p=0.9,
-            n=1,
-        )
-
-            response = response.choices[0].message.content
-    
-        return response
-
-        
-    # Define a function to initialize the chatbot with an initial message
-    def initialize_chat(self):
-        initial_system_message = {"role": "system", 
-                                "content": f"You are a helpful and kind teacher helping a child of age {self.age} learn about a topic\
-                                {self.topic} via the Socratic method.  You are taking on the personality of the kids show 'Paw Patrol'\
-                                character {self.teacher}.  The child's name is {self.kid_name}.  The chat will be question and answer,\
-                                where you will ask a question and the child will respond."}
-        # Add the initial message to the chat history
-        self.chat_messages.append(initial_system_message)
-        self.initial_system_message = initial_system_message
-
-        # Get the initial message from the teacher
-        initial_message_from_teacher = self.get_initial_message_from_teacher()
-        # Add the initial message from the teacher to the chat history
-        self.chat_messages.append({"role": "ai", "content": initial_message_from_teacher})
-        self.chat_history.add_ai_message(initial_message_from_teacher)
-        self.chat_history_dict = self.save_chat_history_dict()
-        # Return the chat history
-        return self.chat_history_dict
-        
-        
-
-    # Define the function to get the answer from the teacher
-    def get_answer_from_teacher(self, response: str):                
-        messages = [
-            {
-                "role": "system",
-                "content": f"You are a teacher that is helping a child learn about different topics based on the socratic method.\
-                    You have taken on the personality of {self.teacher} from the kids show 'Paw Patrol'.  The child's name is {self.kid_name}.\
-                    The topic you are teaching is {self.topic}.  The age of the child is {self.age}.  The format of the conversation is question / response,\
-                    where you ask a question and the child gives a response.\
-                    Your chat history so far is {self.chat_messages}.  Please ask the next question based on the child's response {response}."
-            },
-            {
-                "role": "user",
-                "content": f"{response}"
+                "content": f"You are a teacher that is helping a child learn about different topics based on role playing games.\
+                    You have taken on the personality of {self.teacher} from the kids show 'Paw Patrol' in order to do so.  The child's name is {self.kid_name}.\
+                    The topic you are teaching is {self.topic}.  The age of the child is {self.age}.  Please start the conversation off by asking the\
+                    child a question to initiate the gameplay back and forth.  This is only one question.  The child will then respond with an answer.  Please only\
+                    return the first question, and not a simulated conversation."
             },
         ]
 
-        # Use the OpenAI API to generate a question based on the child's response
-        try:
-            response = openai.ChatCompletion.create(
-            model="gpt-3.5-turbo-16k-0613",
-            messages=messages,
-            max_tokens=750,
-            frequency_penalty=0.5,
-            presence_penalty=0.5,
-            temperature=1,
-            top_p=0.9,
-            n=1,
+        models = ["gpt-3.5-turbo-16k-0613", "gpt-3.5-turbo", "gpt-3.5-turbo-16k"]
+        for model in models:
+            for _ in range(3):  # Retry 3 times
+                try:
+                    response = openai.ChatCompletion.create(
+                        model=model,
+                        messages=messages,
+                        max_tokens=750,
+                        frequency_penalty=0.5,
+                        presence_penalty=0.5,
+                        temperature=1,
+                        top_p=0.9,
+                        n=1,
+                    )
+                    response = response.choices[0].message.content
+                    if response:
+                        st.session_state.chat_messages.append({"role": "ai", "content": response})
+                        return response
+                except (requests.exceptions.RequestException, openai.error.APIError):
+                    continue  # Skip to the next iteration if an exception is caught
+            break  # Break the outer loop if no exception was caught in the inner loop
+
+    def add_message(self, role: str, content: str):
+        formatted_message = {"role": role, "content": content}
+        st.session_state.chat_messages.append(formatted_message)
+        return st.session_state.chat_messages
+        
+        
+    def get_answer_from_teacher(self, kid_response: str):
+        st.session_state.chat_messages.append({"role": "user", "content": kid_response})
+        prompt = PromptTemplate(
+            template = """
+        )           You are a teacher that is helping a child learn about different topics by engaging them in an exciting role playing adventure.
+                    You have taken on the personality of {teacher} from the kids show 'Paw Patrol'.  The child's name is {kid_name}.
+                    The topic you are teaching is {topic}.  The age of the child is {age}.  The format of the conversation is question / response,
+                    where you ask a question and the child gives a response.  The conversation should be an engaging and exciting
+                    adventure for the child that is fun, educational, and age approrpiate.  Generate your response based on the child's response {kid_response}
+                    to your previous question {last_question}.   The conversation history so far is {chat_messages} for reference.
+                    Please continue to guide the child on the learning adventure until they are ready to stop.
+                    Your response should be a JSON object in this format: {format_instructions}
+                    """,
+                    input_variables=["kid_response", "teacher", "kid_name", "topic", "age", "chat_messages", "last_question"],
+                    partial_variables={"format_instructions" : output_parser.get_format_instructions()}
         )
-            question = response.choices[0].message.content
-            # Add the child's response to the chat history
-            self.chat_history.add_user_message(response)
+        system_message_prompt = SystemMessagePromptTemplate(prompt=prompt)
+        human_template = """Please respond to my answer in the format requested"""
+        human_message_prompt = HumanMessagePromptTemplate.from_template(human_template)
+        chat_prompt = ChatPromptTemplate.from_messages([system_message_prompt, human_message_prompt])
+        messages = chat_prompt.format_prompt(last_question = st.session_state.chat_messages[-1], kid_response = kid_response, teacher = self.teacher, topic = self.topic, age = self.age, kid_name = self.kid_name, chat_messages = st.session_state.chat_messages).to_messages()
 
-            # Add the question to the chat history
-            self.chat_history.add_ai_message(question)
-            self.chat_history_dict = self.save_chat_history_dict()
+        models = ["gpt-3.5-turbo-16k-0613", "gpt-3.5-turbo-0613", "gpt-3.5-turbo-16k"]
+        for model in models:
+            try:
+                chat = ChatOpenAI(model_name = model, temperature = 1, max_retries=3)
 
+                teacher_response = chat(messages).content
 
+                parsed_response = output_parser.parse(teacher_response)
 
-        except (requests.exceptions.RequestException, openai.error.APIError):
-            response = openai.ChatCompletion.create(
-            model="gpt-3.5-turbo-16k",
-            messages=messages,
-            max_tokens=750,
-            frequency_penalty=0.5,
-            presence_penalty=0.5,
-            temperature=1,
-            top_p=0.9,
-            n=1,
-        )
+                # Add the question to the chat history
+                st.session_state.chat_messages.append({"role": "ai", "content": parsed_response.text})
+                # Check to see whether or not the user got the question correct
+                
+                return parsed_response.text
+                
+            except (requests.exceptions.RequestException, openai.error.APIError):
+                continue
+            
 
-            question = response.choices[0].message.content
-            # Add the child's response to the chat history
-            self.chat_history.add_user_message(response)
-
-            # Add the question to the chat history
-            self.chat_history.add_ai_message(question)
-            self.chat_history_dict = self.save_chat_history_dict()
-    
-        return self.chat_messages
 
     # Define a function to clear the chat history
     def clear_chat_history(self):
@@ -224,16 +172,17 @@ class ChatService:
         self.chat_history_dict = {}
         self.chat_messages = []
 
-        
+    
 # Define the session variables
 session_vars = [
-        'chat_session',
-    ]
+    'chat_session',
+]
 default_values = [
-        ChatService(),
-    ]
+    ChatService(),
+]
 
 # Loop through the session variables and set them if they are not already set
 for session_var, default_value in zip(session_vars, default_values):
     if session_var not in st.session_state:
         st.session_state[session_var] = default_value
+
